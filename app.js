85db068db76800e7f4ebf962ccf76bf358d11048
Sumit Saurabh <sumitsaurabh927@gmail.com>
2021-08-01 15:39:27 +0530

    took input and console.log it

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var btn = document.querySelector("#btn");
+var txt = document.querySelector("#txt")
 console.log(btn);
 btn.addEventListener("click", function click() {
-    console.log("Magic")
+    console.log("Magic");
+    console.log(txt.value)
 });
+
