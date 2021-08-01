f7d74d186c4b3dbcf251a31850181605266e9759
Sumit Saurabh <sumitsaurabh927@gmail.com>
2021-08-01 16:06:29 +0530

    wired output to user input

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var btn = document.querySelector("#btn");
 var txt = document.querySelector("#txt")
+var out=document.querySelector("#output")
 console.log(btn);
 btn.addEventListener("click", function click() {
     console.log("Magic");
-    console.log(txt.value)
+    // console.log(txt.value);
+    out.innerText=txt.value;
 });
 
